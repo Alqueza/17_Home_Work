5b0b5031013f6d20f9e8616d5f0ba54d857f1336
Otorbaev Adilet <adilet_otor@mail.ru>
2022-02-14 15:24:20 +0600

    done

--- src/Animal/Animal.java
+++ src/Animal/Animal.java
@@ -3,6 +3,14 @@ package Animal;
 public class Animal {
     protected String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Animal(String name) {
         this.name = name;
     }
--- src/Animal/Main.java
+++ src/Animal/Main.java
@@ -1,30 +1,64 @@
 package Animal;
 
+
+import jdk.dynalink.linker.support.TypeUtilities;
+
 public class Main {
     public static void main(String[] args) {
 
         Animal[] animals = {new Shark("Shark"),
-        new Turtle("Turtle"),
-        new Eagle("Eagle")};
+                new Turtle("Turtle"),
+                new Eagle("Eagle")};
 
-        for (Animal a: animals) {
-            System.out.println();
-            System.out.println(a);
-            a.hunting();
-            System.out.println();
+        for (Animal a : animals) {
+            if (a.getClass().getName().equals("Animal.Shark")){
+                System.out.println(a);
+            }
+            if (a instanceof Shark) {
+                ((Shark) a).attack();
+                System.out.println();
+            }
+
+            /*if (a instanceof Shark) {
+                System.out.println(a);
+                ((Shark) a).attack();
+            }*/
+
+            if (a.getClass().getName().equals("Animal.Turtle")) {
+                System.out.println(a);
+            }
+            if (a instanceof Turtle) {
+                ((Turtle) a).swim();
+                System.out.println();
+            }
+
+            /*if (a instanceof Turtle) {
+                System.out.println(a);
+                ((Turtle) a).swim();
+            }*/
+
+            if (a.getClass().getName().equals("Animal.Eagle")) {
+                System.out.println(a);
+            }
+            if (a instanceof Eagle) {
+                ((Eagle) a).fly();
+            }
+
+            /*if (a instanceof Eagle) {
+                System.out.println(a);
+                ((Eagle) a).fly();
+            }*/
+
+//        Shark shark = new Shark("Shark");
+//        shark.attack();
+//        System.out.println();
+//
+//        Turtle turtle = new Turtle("Turtle");
+//        turtle.swim();
+//        System.out.println();
+//
+//        Eagle eagle = new Eagle("Eagle");
+//        eagle.fly();
+            }
         }
-
-
-        Shark shark = new Shark("Shark");
-        shark.attack();
-        System.out.println();
-
-        Turtle turtle = new Turtle("Turtle");
-        turtle.swim();
-        System.out.println();
-
-        Eagle eagle = new Eagle("Eagle");
-        eagle.fly();
-
     }
-}
